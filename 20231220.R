e7c8db8d15fa8a901ff3b66a10038865bf5928b6
LLIB1145\Reader <408222208v@gmail.com>
2023-12-27 14:20:21 +0800

    使控制區的參數隨著Server端的狀況而改變

--- 20231220.R
+++ 20231220.R
@@ -11,3 +11,17 @@ Color = "red"          #指定顏色
 pdf("plot.pdf")      #開啟一個pdf file，路徑為"plot.pdf"
 plot(Data, col = Color) #將圖畫到開啟中的pdf file上去
 dev.off()            #關掉這個pdf file
+
+Result = lm(Weight~Height, data = Data) #linear regression test
+Result
+summary(Result)
+
+plot(Data$Height, Data$Weight, pch = 19)
+abline(Result)
+
+Data=read.table("Example.txt", header = TRUE)
+colnames(Data)    # 列出所有的欄位名稱
+colnames(Data)[3] # 列出第一個欄位名稱
+
+Result1 = lm(Data[,"Weight"]~Data[,"Height"])
+summary(Result1)
