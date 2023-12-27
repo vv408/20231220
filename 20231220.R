38b0f12f48a86bb3bd44b64ae5215a789faa155a
LLIB1145\Reader <408222208v@gmail.com>
2023-12-27 14:22:56 +0800

    換個分析方法

--- 20231220.R
+++ 20231220.R
@@ -25,3 +25,9 @@ colnames(Data)[3] # 列出第一個欄位名稱
 
 Result1 = lm(Data[,"Weight"]~Data[,"Height"])
 summary(Result1)
+
+Result1 = cor.test(Data[,"Weight"], Data[,"Height"], method = "pearson") # Pearson correlation
+Result1
+
+Result2 = cor.test(Data[,"Weight"], Data[,"Height"], method = "spearman") # Spearman correlation
+Result2
